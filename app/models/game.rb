1c61ca9dc00b33ab566fb84b18beb642d52b6a21
James Jannicelli <jjannicelli@gmail.com>
2018-02-04 16:06:57 -0500

    show players on game page

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
-  has_many :players, through: :game_player
+  has_many :game_players
+  has_many :players, :through => :game_players
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
   validates :name, presence: true
+  has_many :game_players
+  has_many :games, :through => :game_players
 end
