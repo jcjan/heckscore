11fe2787d689399a429c4da8f26a7476e3459f98
James Jannicelli <jjannicelli@gmail.com>
2018-02-05 10:16:07 -0500

    add tests for game model

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_max_hand_size
+    g = Game.new(size: 4)
+    assert_equal(12, g.max_hand_size)
+
+    g2 = Game.new(size: 8)
+    assert_equal(6, g2.max_hand_size)
+  end
+
+  def test_hand_count
+    g = Game.new(size: 3)
+    assert_equal(33, g.hand_count)
+
+    g2 = Game.new(size: 7)
+    assert_equal(13, g2.hand_count)
+  end
 end
