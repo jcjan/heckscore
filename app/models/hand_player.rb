d4b1e2341b8202fe86af4f15aba851c38c5996ec
James Jannicelli <jjannicelli@gmail.com>
2018-02-07 09:29:16 -0500

    calculate score of each hand

--- app/models/hand_player.rb
+++ app/models/hand_player.rb
@@ -1,4 +1,28 @@
 class HandPlayer < ApplicationRecord
   belongs_to :hand
   belongs_to :game_player
+
+  def got_set
+    return self.bid != self.tricks
+  end
+
+  def hand_score
+    score = self.tricks
+    if !self.got_set
+      score += 10
+    end
+    return score
+  end
+
+  # the cumulative score up to this point
+  def game_score
+    previous_hands = self.game_player.hand_players.joins(:hand).where(
+      "hands.position <= ?", self.hand.position
+    )
+    gs = 0
+    for h in previous_hands
+      gs += h.hand_score
+    end
+    return gs
+  end
 end
